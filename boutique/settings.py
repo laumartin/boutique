093c89264897483591ca0550ae950bfa37f30ead
laumartin <lauratemar@gmail.com>
2021-03-28 21:59:18 +0000

    sending real emails

--- boutique/settings.py
+++ boutique/settings.py
@@ -128,8 +128,10 @@ AUTHENTICATION_BACKENDS = [
 # when connecting via social media accounts.
 SITE_ID = 1
 
-# Temporarily log emails to the console so we can get the confirmation links.
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''Temporarily log emails to the console so we can get the confirmation links.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' '''
+
+
 
 # tells allauth that we want to allow authentication using either
 # usernames or emails
@@ -262,4 +264,16 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutique@example.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutique@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
