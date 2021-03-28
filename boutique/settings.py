ea5cfc006fd44e0fbeccd8b7d03cc29e5df65f00
laumartin <lauratemar@gmail.com>
2021-03-28 18:32:28 +0000

    Added cache control

--- boutique/settings.py
+++ boutique/settings.py
@@ -220,6 +220,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # To connect Django with S3 and tell it which AWS bucket it should
 # be communicating with
 if 'USE_AWS' in os.environ:
+    # Cache control. This will tell the browser that it's okay to
+    # cache static files for a long time since they don't change
+    # very often, and this will improve performance for our users.
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'lau-boutique'
     AWS_S3_REGION_NAME = 'eu-west-2'
